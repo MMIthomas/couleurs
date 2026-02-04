e8d1e76bd5fb9372a793750537619a0fd0c4eb4f
vayd0 <kiiroxyz@gmail.com>
2026-02-04 17:10:34 +0100

    Ajout: Début animations et font

--- src/components/red/Red.tsx
+++ src/components/red/Red.tsx
@@ -1,3 +1,64 @@
+import "./red.scss";
+import { useEffect } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { SplitText } from "gsap/SplitText";
+
+gsap.registerPlugin(SplitText, ScrollTrigger);
+
 export default function Red() {
-  return <div></div>;
+  useEffect(() => {
+    const wrapper = document.querySelector(".Horizontal");
+    const text = document.querySelector(".Horizontal__text");
+    if (!wrapper || !text) return;
+
+
+    const split = SplitText.create(text, { type: "chars,words" });
+
+    const container = document.querySelector('.container');
+    if (!container) return;
+    const scrollTween = gsap.to(container, {
+      xPercent: -80,
+      ease: "none",
+      scrollTrigger: {
+        trigger: wrapper,
+        pin: true,
+        scrub: true,
+        end: () => `+=${container.scrollWidth}`,
+        anticipatePin: 1,
+      },
+    });
+
+    split.chars.forEach((char) => {
+      gsap.from(char, {
+        yPercent: gsap.utils.random(-200, 200),
+        rotation: gsap.utils.random(-20, 20),
+        ease: "back.out(1.2)",
+        scrollTrigger: {
+          trigger: char,
+          containerAnimation: scrollTween,
+          start: "left 100%",
+          end: "left 30%",
+          scrub: 1,
+        },
+      });
+    });
+
+    return () => {
+      ScrollTrigger.getAll().forEach((st) => st.kill());
+      if (split && split.revert) split.revert();
+    };
+  }, []);
+
+  return (
+    <div>
+      <section className="Horizontal">
+        <div className="container">
+          <h3 className="Horizontal__text heading-xl">
+            Le rouge a toujours 
+          </h3>
+        </div>
+      </section>
+    </div>
+  );
 }
