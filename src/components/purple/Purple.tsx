49e16e8b7cd7597736630a3f966dba306c47ef86
Thomas Seyroles <seytom00@gmail.com>
2026-02-11 16:14:38 +0100

    feat: Introduce GSAP ScrollTrigger for title animation, add a new motif section, and refine component styling.

--- src/components/purple/Purple.tsx
+++ src/components/purple/Purple.tsx
@@ -4,8 +4,9 @@ import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { SplitText } from "gsap/SplitText";
 import ScrollBar from "./scrollBar/ScrollBar";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-gsap.registerPlugin(useGSAP, SplitText);
+gsap.registerPlugin(useGSAP, SplitText, ScrollTrigger);
 
 export default function Purple() {
   const container = useRef<HTMLDivElement>(null);
@@ -17,20 +18,52 @@ export default function Purple() {
       const splitText = new SplitText(colorTitle.current, {
         type: "chars",
       });
-      gsap.from(splitText.chars, {
-        y: 150,
-        duration: 1,
-        ease: "power3.out",
-        stagger: 0.02,
+
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: container.current,
+          start: "top top",
+          end: () =>
+            `+=${container.current ? container.current.offsetHeight / 6 : 0}`,
+          pin: colorTitleContainer.current,
+          scrub: 1,
+          markers: true,
+        },
       });
+
+      tl.from(splitText.chars, {
+        y: 150,
+        ease: "power3.out",
+        stagger: 0.05,
+        duration: 1,
+      }).to(
+        splitText.chars,
+        {
+          y: 150,
+          ease: "power3.in",
+          stagger: 0.05,
+          duration: 1,
+        },
+        "+=1",
+      );
     },
     { scope: container },
   );
 
   return (
     <section className={styles.purple} ref={container}>
-      <div className={styles.purple__title} ref={colorTitleContainer}>
-        <h2 ref={colorTitle}>Violet</h2>
+      <div className={styles.purple__title}>
+        <div
+          className={styles.purple__title__wrapper}
+          ref={colorTitleContainer}
+        >
+          <h2 ref={colorTitle}>Violet</h2>
+        </div>
+      </div>
+      <div className={styles.purple__motif}>
+        <div className={styles.purple__motif__wrapper}>
+          <span>Violet</span>
+        </div>
       </div>
       <ScrollBar container={container} />
     </section>
